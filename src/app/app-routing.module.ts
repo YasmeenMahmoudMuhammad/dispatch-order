5f02c91bd8e212ba3b71bf21ec363440ceae46c7
yasmin <y.mahmoud>
2023-07-26 21:18:11 +0300

    changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [];
+import { HomComponent } from './hom/hom.component';
+const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full'},
+  {path: 'home' , component:HomComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,32 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomComponent } from './hom/hom.component';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { RemixIconModule, RiAncientGateFill, RiCalendarLine, RiHome2Fill } from "angular-remix-icon";
+import{RiStarFill} from "angular-remix-icon";
+import {MatRadioModule} from '@angular/material/radio'; 
+import {MatButtonToggleModule} from '@angular/material/button-toggle';
+const icons = {
+  RiAncientGateFill,
+  RiHome2Fill,
+  RiStarFill,
+  RiCalendarLine
+};
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NoopAnimationsModule,
+    RemixIconModule.configure(icons),
+    MatRadioModule,
+    MatButtonToggleModule
   ],
   providers: [],
   bootstrap: [AppComponent]
