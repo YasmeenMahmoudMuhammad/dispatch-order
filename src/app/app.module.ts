a4c702010ee50f24c497b48409c11f1dbd2159bd
yasmin <y.mahmoud>
2023-07-27 14:19:53 +0300

    new change

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomComponent } from './hom/hom.component';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
-import { RemixIconModule, RiAncientGateFill, RiCalendarLine, RiHome2Fill } from "angular-remix-icon";
+import { RemixIconModule, RiAncientGateFill, RiArrowRightSLine, RiCalendarLine, RiHome2Fill } from "angular-remix-icon";
 import{RiStarFill} from "angular-remix-icon";
 import {MatRadioModule} from '@angular/material/radio'; 
 import {MatButtonToggleModule} from '@angular/material/button-toggle';
@@ -12,7 +12,8 @@ const icons = {
   RiAncientGateFill,
   RiHome2Fill,
   RiStarFill,
-  RiCalendarLine
+  RiCalendarLine,
+  RiArrowRightSLine
 };
 
 @NgModule({
